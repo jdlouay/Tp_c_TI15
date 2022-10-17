3b27458f39a665e9c2cea5dd206a558c95ec4088
JDAY FIRAS <jdalouay@gmail.com>
2022-10-18 00:34:16 +0100

    Create ex4.1.2.c

---
+++ tp4/ex4.1.2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{
+	int x, q, r, i, j;
+	int b[10];
+	printf("donne un entier positif : ");
+	scanf("%d", &x);
+	i = 1;
+	q = x;
+	do
+	{
+		b[i] = q % 2;
+		q = q / 2;
+		i++;
+	} while (q > 0);
+
+	printf("Donc le nombre decimal %d vaut ", x);
+	for (j = 1; j <= 4; j++)
+	{
+		printf("%d", b[j]);
+	}
+	printf(" en binaire.");
+}
